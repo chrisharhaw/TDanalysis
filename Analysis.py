29d547a0ca9df00923da2aaf928fe45f1f49c0a6
Christopher Harvey-Hawes <153693046+chrisharhaw@users.noreply.github.com>
2024-06-10 11:00:10 +1200

    updates

--- Analysis.py
+++ Analysis.py
@@ -1,10 +1,23 @@
 import numpy as np
+import os
+import logging
+import sys
+import math
 from astropy.cosmology import FlatLambdaCDM
 import pymultinest
 
 #load the catalogue
 from TDcatalogue import catalogue, c
 
+# Set up logging
+logging.basicConfig(level=logging.DEBUG, 
+                    format='%(asctime)s %(levelname)s:%(message)s',
+                    handlers=[
+                        logging.FileHandler("debug.log"),
+                        logging.StreamHandler(sys.stdout)
+                    ])
+
+logger = logging.getLogger()
 
 
 class loglike_flrw(object):
@@ -18,10 +31,11 @@ class loglike_flrw(object):
         self.td = td
         self.sigmatd = sigmatd
         self.Om0 = None
-        self.c = c
+        self.ddt = ddt
+        self.sigmaddt = sigmaddt
 
     def __call__(self, cube, ndim, nparams):
-        params = {'Om0': cube[0]}
+        params = {'Om0': float(cube[0])}
         self.cosmo = FlatLambdaCDM(H0 = 70, **params)
         return self.ln_likelihood_flrw()
     
@@ -30,23 +44,29 @@ class loglike_flrw(object):
         Returns the log likelihood of the data given the model parameters.
         """
         model_predictions = self.ddt_flrw()
-        residuals = self.ddt() - model_predictions
-        return -0.5 * np.sum(residuals**2 / self.sigmaddt()**2)
+        residuals = self.ddt - model_predictions
+        return -0.5 * np.sum((residuals / self.sigmaddt)**2)
 
     def ddt_flrw(self):
-        R = self.cosmo.angular_diameter_distance(self.zs) * self.cosmo.angular_diameter_distance(self.zl)/ self.cosmo.angular_diameter_distance_z1z2(self.zl, self.zs)
+        R = np.empty(len(self.zs))
+        for i in range(len(self.zs)):
+            Rs = self.cosmo.angular_diameter_distance(self.zs[i]).value
+            Rl = self.cosmo.angular_diameter_distance(self.zl[i]).value 
+            Rls = self.cosmo.angular_diameter_distance_z1z2(self.zl[i], self.zs[i]).value
+            R[i] = Rs * Rl / Rls
         return (1 + self.zl) * R
-
-    def ddt(self):
-        return 2 * self.c * self.td / (self.thetaB**2 - self.thetaA**2)
     
-    def sigmaddt(self): ##Check this properly!!!!!!
-        return self.ddt() * np.sqrt((self.sigmatd / self.td)**2 + (2 * self.sigmaA / self.thetaA)**2 + (2 * self.sigmaB / self.thetaB)**2)
-    
-ndim = 1
-prior_lims = [(0.0, 0.5)]
 
 zl, zs, thetaA, sigmaA, thetaB, sigmaB, td, sigmatd = catalogue()
+
+ddt = abs((2 * c * td / (thetaB**2 - thetaA**2))) * 3.2408e-17 / 1e6
+sigmaddt = np.zeros(len(zl))
+for i in range(len(zl)):
+    sigmaddt[i] = ddt[i] * np.sqrt((sigmatd[i] / td[i])**2 + (2 * sigmaA[i] / thetaA[i])**2 + (2 * sigmaB[i] / thetaB[i])**2)
+
+
+ndim = 1
+prior_lims = [(0.0, 0.5)]
 loglike = loglike_flrw(zl, zs, thetaA, sigmaA, thetaB, sigmaB, td, sigmatd)
 
 def prior(cube, ndim, nparams):
@@ -58,15 +78,23 @@ def llikenargs(cube, ndim, nparams):
 
 tol = 1e-3
 nlive = 800
-folder = '/Bayes/'
+folder = '/home/users/cha227/Documents/TDanalysis-main/Bayes/FLRW/'
 basename = 'flrw_' + str(nlive) + '_' + str(tol) + '_'
 
-pymultinest.run(llikenargs, prior, ndim,
-                outputfiles_basename = folder+basename,
-                multimodal = False,
-                sampling_efficiency = 'model',
-                n_live_points = nlive,
-                evidence_tolerance = tol,
-                const_efficiency_mode = False,
-                n_iter_before_update = 8000,
-                verbose = True)
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+try:
+    pymultinest.run(llikenargs, prior, ndim,
+                    outputfiles_basename = folder+basename,
+                    multimodal = False,
+                    sampling_efficiency = 'model',
+                    n_live_points = nlive,
+                    evidence_tolerance = tol,
+                    const_efficiency_mode = False,
+                    n_iter_before_update = 8000,
+                    verbose = True)
+    
+except Exception as e:
+    logger.exception("An error occurred")
+
